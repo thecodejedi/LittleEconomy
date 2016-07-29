110d97822399fbbffabd8e89b45a5279f152e698
thecodejedi <thecodejedi@gmail.com>
2016-07-30 00:07:26 +0200

    add Map Generators

---
+++ Assets/MainGame/Scripts/MoveCamera.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCamera : MonoBehaviour {
+
+	public int speed;
+	void Update()
+	{
+		if(Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.Translate(new Vector3(speed * Time.deltaTime,0,0));
+		}
+		if(Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.Translate(new Vector3(-speed * Time.deltaTime,0,0));
+		}
+		if(Input.GetKey(KeyCode.DownArrow))
+		{
+			transform.Translate(new Vector3(0,-speed * Time.deltaTime,0));
+		}
+		if(Input.GetKey(KeyCode.UpArrow))
+		{
+			transform.Translate(new Vector3(0,speed * Time.deltaTime,0));
+		}
+	}
+}
---
+++ Assets/MainGame/Scripts/Tile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile
+{
+	public IEnumerable<Vector3> CreateVector ()
+	{
+		yield return new Vector3 (x, y);
+		yield return new Vector3 (x+1, y);
+		yield return new Vector3 (x+1, y+1);
+		yield return new Vector3 (x, y+1);
+		yield return new Vector3 (x, y);
+
+	}
+
+	public int x;
+
+	public int y;
+
+	public Tile (int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+}
+
