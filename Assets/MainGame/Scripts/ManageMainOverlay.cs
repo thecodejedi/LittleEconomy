08635b03597a0429394c49258a592fcd7f9b43cd
thecodejedi <thecodejedi@gmail.com>
2016-07-29 22:49:40 +0200

    initial Unity Project with Menu

---
+++ Assets/MainGame/Scripts/ManageMainOverlay.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ManageMainOverlay : MonoBehaviour {
+
+	public Canvas mainMenuCanvas;
+
+	// Use this for initialization
+	void Start () {
+		mainMenuCanvas = mainMenuCanvas.GetComponent<Canvas> ();
+		HideMenu ();
+	}
+
+	public void DisplayMenu(){
+		mainMenuCanvas.enabled = true;
+	}
+
+	public void HideMenu(){
+		mainMenuCanvas.enabled = false;
+	}
+
+	public void ReturnToMainMenu(){
+		SceneManager.LoadScene ("StartMenu");
+	}
+
+	public void ExitToDesktop(){
+		Application.Quit();
+	}
+}
---
+++ Assets/StartMenu/Scripts/MenuScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+	public Button exitButton;
+	public Button startButton;
+
+	// Use this for initialization
+	void Start () {
+		startButton = startButton.GetComponent<Button> ();
+		exitButton = exitButton.GetComponent<Button> ();
+	}
+	
+	public void ExitPress(){
+		Application.Quit ();
+	}
+
+	public void StartPress(){
+		SceneManager.LoadScene ("MainGame");
+	}
+}
